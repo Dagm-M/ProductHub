baa252774de699029e111d8ce9d2ff370880d471
Dagmawi Muluwork <dagmawimuluwork@gmail.com>
2023-09-22 19:38:55 +0300

    basic logic done

---
+++ Application/DTO/ProductDTO/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTO.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.ProductDTO
+{
+    public class ProductDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Pricing { get; set; }
+        public int Availability { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ Application/Features/Product/Requests/Querie/GetProductsByCategoryIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using Application.DTO.ProductDTO;
+using Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Product.Requests.Querie
+{
+    public class GetProductsByCategoryIdRequest : IRequest<BaseCommandResponse<List<ProductDto>>>
+    {
+        public int CategoryId { get; set; }
+    }
+}
